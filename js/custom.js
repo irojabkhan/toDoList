0ca55aa0fb9a6f215960da8e76e2f99444fcd053
irojabkhan <irojabkhan@gmail.com>
2019-05-01 03:11:07 +0600

    Taskbox Created

--- js/custom.js
+++ js/custom.js
@@ -8,18 +8,46 @@ window.onload = function() {
 	const allTalk = document.getElementById('allTask')
 
 
+	// Create Task
 	taskField.addEventListener('keypress', function(event) {
-		if(event.keyCode === 13) {
+		if(event.keyCode === 13 && this.value != '') {
 			creatTask(allTalk, event.target.value)
 			this.value = ''
 		}
 	})
 
-	function creatTask(parent, task) {
-		console.log(task)
+	// addTaskBtn.addEventListener('click', function(event) {
+	// 	if(event.target.value != '') {
+	// 		creatTask(allTalk, event.target.value)
+	// 		this.value = ''
+	// 	}
+	// })
+
+	// Single Task Create 
+	function creatTask(allTaskParent, task) {
+
+		let col = document.createElement('div')
+		col.setAttribute('class', 'col-lg-4 col-md-6')
+
+		let singleTask = document.createElement('div')
+		singleTask.setAttribute('class', 'single-task')
+
+		let singleTaskP = document.createElement('p')
+
+		let span = document.createElement('span')
+		span.innerHTML = '<i class="fas fa-times-circle"></i>'
+		span.addEventListener('click', function() {
+			allTaskParent.removeChild(col)
+		})
+
+		singleTaskP.innerHTML = task
+
+		singleTask.appendChild(singleTaskP)
+		singleTask.appendChild(span)
+		col.appendChild(singleTask)
+		allTaskParent.appendChild(col)
+
 	}
-
-
 
 
 
